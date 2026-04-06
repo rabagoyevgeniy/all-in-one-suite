bdebcac85f7dc4bdf0f3c14fd31b1a7d2a08122c
Yevgeniy <Rabaqo.yevgeniy@gmail.com>
2026-04-06 19:25:52 +0400

    fix: hardcode posthog key and host to ensure events are sent on production

--- src/posthog.ts
+++ src/posthog.ts
@@ -1,10 +1,12 @@
 import posthog from "posthog-js";
 
-posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
-  api_host: import.meta.env.VITE_POSTHOG_HOST,
+posthog.init("phc_NA8yuj00wzGXqhafsxdnjgemdCHxa9FwYF2cjhbt7DZ", {
+  api_host: "https://eu.i.posthog.com",
   capture_pageview: false,
   capture_pageleave: true,
   persistence: "localStorage+cookie",
 });
+
+console.log("[PostHog] initialized", posthog.get_distinct_id());
 
 export default posthog;
